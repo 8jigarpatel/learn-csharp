fccdeccc21ccfe69f34319600e4b99ad2e3a481a
Jigar Patel <8.jigar.patel@gmail.com>
2023-04-27 15:06:49 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,40 +5,30 @@ internal class Program
     enum Run
     {
         Undefined,
+        Arrays,
         Operators,
-        Arrays
+        TypeConversions
     }
 
-    private static Run run = Run.Operators;
+    private static Run run = Run.TypeConversions;
 
     private static void Main(string[] args)
     {
         switch (run)
         {
+            case Run.Arrays:
+                Arrays();
+                break;
             case Run.Operators:
                 Operators();
                 break;
-            case Run.Arrays:
-                Arrays();
+            case Run.TypeConversions:
+                TypeConversions();
                 break;
             default:
                 Console.WriteLine("Please update `run` parameter.");
                 break;
         };
-        Console.WriteLine("END");
-    }
-
-    private static void Operators()
-    {
-        int numerator = 25;
-        int denominator = 3;
-
-        int quotient = numerator / denominator;
-        int remainder = numerator % denominator;
-
-        Console.WriteLine("numerator: {0}, and denominator: {1}", numerator, denominator);
-        Console.WriteLine("quotient: {0}", quotient);
-        Console.WriteLine("remainder: {0}", remainder);
     }
 
     private static void Arrays()
@@ -159,5 +149,70 @@ internal class Program
         Console.WriteLine(string.Join(",", myNums)); // 1,2,3,4,5
         Array.Reverse(myNums);
         Console.WriteLine(string.Join(",", myNums)); // 5,4,3,2,1
+    }
+
+    private static void Operators()
+    {
+        int numerator = 25;
+        int denominator = 3;
+
+        int quotient = numerator / denominator; // 8 (8*3 = 24)
+        int remainder = numerator % denominator; // 1 (24+1 = 25)
+
+        Console.WriteLine("numerator: {0}, and denominator: {1}", numerator, denominator);
+        Console.WriteLine("quotient: {0}", quotient);
+        Console.WriteLine("remainder: {0}", remainder);
+
+        int? problems = null;
+        Console.WriteLine("There are {0} problems.", problems ?? 0); // There are 0 problems
+    }
+
+    private static void TypeConversions()
+    {
+        // Implicit - two conditions must satisfy
+        //    1. no loss of information
+        //    2. no possibility of exception
+        // e.g., from int to float
+        int i1 = 100;
+        float f1 = i1; // 100
+        Console.WriteLine(f1); 
+
+        // Explicit - two conditions aren't met
+        // e.g., from float to int
+        float f2 = 555.55F;
+        // int i2 = f2; // Cannot implicitly convert type 'type1' to 'type2'. An explicit conversion exists (are you missing a cast?)
+        int i2 = (int)f2; // 555
+        Console.WriteLine(i2);
+        int i22 = Convert.ToInt32(f2); // 556
+        Console.WriteLine(i22);
+        
+        float f3 = 555555555555;
+        int i3 = (int)f3; // -2147483648
+        Console.WriteLine(i3);
+        // int i33 = Convert.ToInt32(f3); // System.OverflowException: 'Value was either too large or too small for an Int32.'
+
+        string validIntStr = "999";
+        int validInt = Convert.ToInt32(validIntStr); // 999
+        string invalidIntStr = "999.99";
+        // int invalidInt = Convert.ToInt32(invalidIntStr); // System.FormatException: 'Input string was not in a correct format.'
+
+        // Parse & TryParse
+        string intStr1 = "700";
+        int intFromStrParse1 = int.Parse(intStr1); // 700
+        bool tryParseSuccess1 = int.TryParse(intStr1, out int intFromStrTryParse1);
+        Console.WriteLine(tryParseSuccess1); // false
+        Console.WriteLine(intFromStrTryParse1); // 0
+
+        string intStr2 = "200Blah";
+        // int intFromStrParse2 = int.Parse(intStr2); // System.FormatException: 'Input string was not in a correct format.'
+        bool tryParseSuccess2 = int.TryParse(intStr2, out int intFromStrTryParse2);
+        Console.WriteLine(tryParseSuccess2); // false
+        Console.WriteLine(intFromStrTryParse2); // 0
+
+        string intStr3 = "999999999999";
+        // int intFromStrParse3 = int.Parse(intStr3); // System.OverflowException: 'Value was either too large or too small for an Int32.'
+        bool tryParseSuccess3 = int.TryParse(intStr3, out int intFromStrTryParse3);
+        Console.WriteLine(tryParseSuccess3); // false
+        Console.WriteLine(intFromStrTryParse3); // 0
     }
 }
